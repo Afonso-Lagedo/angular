bb1a2e004f1ba556d623317f14328e21041e97a7
Afonso-lagedo <afonso.lagedo@gmail.com>
2023-03-18 18:24:02 -0300

    Interpolação

--- angular/src/app/componentes/primeiro/primeiro.component.ts
+++ angular/src/app/componentes/primeiro/primeiro.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./primeiro.component.css']
 })
 export class PrimeiroComponent {
-
+  nome: string = 'Afonso';
+  idade: number = 29;
+  trabalho: string = 'Desenvolvedor';
+  linguagens = ['JS', 'PHP'];
+  objeto = {
+    nome: "objeto 1",
+    teste: "OK"
+  }
 }
